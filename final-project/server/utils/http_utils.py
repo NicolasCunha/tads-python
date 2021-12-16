033733847bb21cbfecd93f27ceb184ebe5988905
NicolasCunha <nicolascunha17@gmail.com>
2021-12-16 20:07:52 -0300

    chore: port final project to another repo

--- final-project/server/utils/http_utils.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify
-
-def create_response(dict, status):
-    response = jsonify(dict, status)
-    response.status = status
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
